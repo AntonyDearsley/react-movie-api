c9235b5496497fe62b56affb0683db91e669fd40
Antony <antony.fernandez.dearsley@gmail.com>
2022-04-25 00:32:35 +0200

    changing service to getAll

--- src/hooks/useFilms/index.js
+++ src/hooks/useFilms/index.js
@@ -1,15 +1,19 @@
 import { useEffect, useState } from 'react'
-import getFilms from '../../services/getFilms'
+import getAll from '../../services/getAll'
 
 export function useFilms ({ keyword }) {
     const [films, setFilms] = useState([])
     const [page] = useState(1)
 
     useEffect(() => {
-        getFilms({ keyword , page })
+        getAll({ keyword , page })
             .then(films =>{
-              console.log(films)
-              setFilms(films)
+              const filter = films.filter(film => film.Type === ('movie'))
+              setFilms(filter)
+              
+              setTimeout(() => {
+                window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
+              }, 500)
             }) 
     }, [keyword, page]) 
 
