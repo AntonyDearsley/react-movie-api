d3e4df15832fd93c0519dea22e2719c5a7884173
Antony <antony.fernandez.dearsley@gmail.com>
2022-04-24 19:11:12 +0200

    adding buttom to adding movies

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React, { useState } from 'react'
 import './App.css';
 import ListOfMovies from './components/ListOfMovies';
-// import getFilms from './services/getFilms';
+import getFilms from './services/getFilms';
+import {useFilms} from "./hooks/useFilms"
 
 
 export default function App() {
-  const [keyword] = useState(['Pirates of the caribbean'])
+  const [keyword] = useState(['star wars'])
+  const {films, setFilms} = useFilms({ keyword })
+  const [page, setPages] = useState(2)
   
-  // const [films, setFilms] = useState([])
+  const handleClick = () => {
+    setPages(page+1)
+    getFilms({ keyword , page })
+    .then(film => {
+      const movies = films.concat(film)
+      setFilms(movies)
+    }) 
+  }
+  
 
-  // useEffect(() => { 
-  //     getFilms({ keyword })
-  //         .then(films => {
-  //             setFilms(films)
-  //         })
-  // }, [keyword])
-
-  return <div id='container' className='bg-zinc-900 h-full text-white'>
-    <ListOfMovies keyword={keyword} />
+  return <div id='container' className='bg-zinc-900 h-full text-white flex flex-col items-center'>
+    <div className='container'>
+      <ListOfMovies films={films} />
+    </div>
+    <div>
+      <button id='btn' onClick={handleClick} className='bg-amber-500 text-stone-900 font-bold w-56 p-4 mt-5 mb-7 rounded-md hover:bg-amber-400 hover:scale-105 duration-100' >Más Películas</button>
+    </div>
   </div>
 }
