a8d6cce933e23759e138a11fc6252914ba95fdea
Antony <antony.fernandez.dearsley@gmail.com>
2022-04-24 19:58:13 +0200

    adding auto buttom scroll

--- src/App.js
+++ src/App.js
@@ -2,30 +2,38 @@ import React, { useState } from 'react'
 import './App.css';
 import ListOfMovies from './components/ListOfMovies';
 import getFilms from './services/getFilms';
-import {useFilms} from "./hooks/useFilms"
+import { useFilms } from "./hooks/useFilms";
 
 
 export default function App() {
   const [keyword] = useState(['star wars'])
   const {films, setFilms} = useFilms({ keyword })
   const [page, setPages] = useState(2)
-  
+
   const handleClick = () => {
     setPages(page+1)
     getFilms({ keyword , page })
     .then(film => {
       const movies = films.concat(film)
       setFilms(movies)
-    }) 
+    })
+
+    setTimeout(() => {
+      window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
+    }, 500)
+
   }
   
 
   return <div id='container' className='bg-zinc-900 h-full text-white flex flex-col items-center'>
     <div className='container'>
-      <ListOfMovies films={films} />
-    </div>
+        <ListOfMovies films={films} />
+    </div>    
     <div>
-      <button id='btn' onClick={handleClick} className='bg-amber-500 text-stone-900 font-bold w-56 p-4 mt-5 mb-7 rounded-md hover:bg-amber-400 hover:scale-105 duration-100' >Más Películas</button>
+      <button onClick={handleClick} className='bg-amber-500 text-stone-900 
+      font-bold w-56 p-4 mt-5 mb-7 rounded-md hover:bg-amber-400 hover:scale-105 duration-100' >
+        Más Películas
+      </button>
     </div>
   </div>
 }
