d14d926a68d24b6e5aeaaa6fe7802ccf390aa182
Antony <antony.fernandez.dearsley@gmail.com>
2022-04-25 02:08:31 +0200

    refactor useFilms

--- src/hooks/useFilms/index.js
+++ src/hooks/useFilms/index.js
@@ -1,22 +1,23 @@
 import { useEffect, useState } from 'react'
 import getAll from '../../services/getAll'
 
+
 export function useFilms ({ keyword }) {
-    const [films, setFilms] = useState([])
+    const [parameter, setParameter] = useState([])
     const [page] = useState(1)
+    const type = 'movie'
 
     useEffect(() => {
-        getAll({ keyword , page })
-            .then(films =>{
-              const filter = films.filter(film => film.Type === ('movie'))
-              setFilms(filter)
+        getAll({ keyword , page , type})
+            .then(elements => {
+              setParameter(elements)
               
               setTimeout(() => {
                 window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
               }, 500)
             }) 
-    }, [keyword, page]) 
+    }, [keyword, page, ]) 
 
 
-    return {films, setFilms};
+    return {parameter, setParameter};
 }
