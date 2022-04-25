a45aa35f6056d8b33b2be2bd316289a6e7955084
Antony <antony.fernandez.dearsley@gmail.com>
2022-04-25 02:10:56 +0200

    Refactor getAll adding Type

--- src/services/getAll/index.js
+++ src/services/getAll/index.js
@@ -1,7 +1,8 @@
-const apiKey = "e17f44d2";
+const apiKey = "e17f44d2"
+const url = "https://cdn.dribbble.com/users/547471/screenshots/3063720/not_found.gif"
 
-export default async function getAll ({ keyword, page }) {
-    const apiURL = `http://www.omdbapi.com/?apikey=${apiKey}&s=${keyword}&page=${page}`
+export default async function getAll ({ keyword, page, type }) {
+    const apiURL = `http://www.omdbapi.com/?apikey=${apiKey}&s=${keyword}&type=${type}&page=${page}`
 
     return fetch(apiURL)
       .then(res => res.json())
@@ -9,8 +10,11 @@ export default async function getAll ({ keyword, page }) {
         const films =  response.Search
         films.map(result => { 
             const { Title, imdbID, Poster } = result
-            return { Title, imdbID, Poster }
+            return Poster === 'N/A' ? { Title, imdbID, Poster: url } 
+            : { Title, imdbID, Poster }
         })
         return films
     })
 }
+
+// example http://www.omdbapi.com/?apikey=e17f44d2&s=starwars&page=1
