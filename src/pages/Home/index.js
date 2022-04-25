b8dbb18414ec75b908d6ed782bf1c7b33a409acd
Antony <antony.fernandez.dearsley@gmail.com>
2022-04-25 16:00:28 +0200

    Refactor pushLocation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     const handleSubmit = e => {
         e.preventDefault()
         keyword === '' ? pushLocation('/404') 
-        : pushLocation(`/searchFilm/${keyword}`)
+        : pushLocation(`/search/${keyword}`)
     }
 
     const handleChange = e => {
