e8bd7f4954de004248f8653a7c676df542b98cc7
Antony <antony.fernandez.dearsley@gmail.com>
2022-04-25 15:54:27 +0200

    Creating custom hook useAll

---
+++ src/hooks/useAll/index.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react'
+import getAll from '../../services/getAll'
+
+export function useAll({ keyword }) {
+    const [parameter, setParameter] = useState([])
+    const [page] = useState(1)
+
+    useEffect(() => {
+        getAll({ keyword , page })
+            .then(elements => {
+              setParameter(elements)
+              
+              setTimeout(() => {
+                window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
+              }, 500)
+            }) 
+    }, [keyword, page]) 
+
+
+    return {parameter, setParameter};
+}
