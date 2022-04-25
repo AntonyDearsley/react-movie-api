002fb16bd42e73eb42640e01b0a6033ca90c1ab7
Antony <antony.fernandez.dearsley@gmail.com>
2022-04-25 02:06:15 +0200

    adding comit to refactor

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import { Route } from "wouter"
 import Home from "./pages/Home"
 import SearchFilms from './pages/SearchFilms';
 import Error404 from './pages/404';
+import SearchSeries from './pages/SearchSeries';
 
 export default function App() {
   
+  /*
+        Refactorizar los hooks para pasar el type por parametros
+
+        Ruta a 404 si no existe
+
+        Crear searchAll para input principal
+
+        Hooks de type game
+
+        Arreglar getAll poster not found
+  */
   return <>
       <Route
           component={Home}
@@ -17,6 +29,12 @@ export default function App() {
         component={SearchFilms}
         path="/searchFilm/:keyword" 
       />
+
+      <Route
+        component={SearchSeries}
+        path="/searchSerie/:keyword" 
+      />
+
 
       <Route
         component={Error404}
