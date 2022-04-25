b32ed0b4f7d1dbede502bb5b8da37280bc716baa
Antony <antony.fernandez.dearsley@gmail.com>
2022-04-25 15:51:49 +0200

    Creating Element component

---
+++ src/components/Element/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Element (params) {
+    return <div id={params.id}>
+        <img 
+        className='poster' 
+        src={params.image} 
+        alt={`Element-${params.type}: ${params.title}`} 
+        />
+    </div>
+}
+
