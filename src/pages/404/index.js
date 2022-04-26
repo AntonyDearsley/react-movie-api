75d008b3c9ed889d7d16dac2ba00d78f0502271e
Antony <antony.fernandez.dearsley@gmail.com>
2022-04-26 23:39:17 +0200

    Modify bg of page 404

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -3,8 +3,7 @@ import React from 'react'
 const url404 = "https://cdn.dribbble.com/users/547471/screenshots/3063720/not_found.gif"
 
 export default function Error404() {
-    return  <div id='container' className='bg-neutral-900 h-screen flex justify-center items-center'>
+    return  <div id='container' className='bg-[#1b1b1b] h-screen flex justify-center items-center'>
                 <img src={url404} alt="error 404" />
             </div>
-  
 }
