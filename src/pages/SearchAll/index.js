dbd16a0a1ceb3abc3804b6c87f74c2d00f9d4379
Antony <antony.fernandez.dearsley@gmail.com>
2022-04-26 23:42:24 +0200

    Refactor name function, adding 404page

--- src/pages/SearchAll/index.js
+++ src/pages/SearchAll/index.js
@@ -1,18 +1,24 @@
 import ListOfElements from '../../components/ListOfElements'
 import Button from '../../components/Button'
 import { useHandleClickAll } from '../../hooks/handleClickAll'
+import Error404 from '../404'
 
-export default function SearchType({ params }) {
+export default function SearchAll({ params }) {
     const keyword  = params
     const {handleClick, parameter} = useHandleClickAll(keyword)
-    
-    return  <div id='container' className='bg-zinc-900 h-full text-white flex flex-col items-center'>
-                <div className='container'>
-                    <ListOfElements element={parameter} />
-                </div>
-                    
-                <div>
-                    <Button handleClick={handleClick}>Más Elementos</Button>
-                </div>
+
+    if (parameter === "False" ) {
+        return <Error404/>
+    } 
+
+    return <div id='container' className='bg-zinc-900 h-full text-white flex flex-col items-center'>
+            <div className='container'>
+                <ListOfElements element={parameter} />
             </div>
+                
+            <div>
+                <Button handleClick={handleClick}>Más Elementos</Button>
+            </div>
+        </div>
 }
+    
