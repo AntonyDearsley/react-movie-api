09de4bdd4d955c2e531435ee10299ceacbf9e6e8
Antony <antony.fernandez.dearsley@gmail.com>
2022-04-26 23:37:30 +0200

    Deleting comments

--- src/App.js
+++ src/App.js
@@ -7,18 +7,6 @@ import SearchType from './pages/SearchType'
 import Error404 from './pages/404'
 
 export default function App() {
-  
-  /*
-        Refactorizar los hooks para pasar el type por parametros
-
-        Ruta a 404 si no existe
-
-        Crear searchAll para input principal
-
-        Hooks de type game
-
-        Arreglar getAll poster not found
-  */
   return <>
       <Route
           component={Home}
@@ -28,6 +16,7 @@ export default function App() {
       <Route
         component={SearchAll}
         path="/search/:keyword" 
+       
       />
 
       <Route
