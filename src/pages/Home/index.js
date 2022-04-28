f67edd0d331addbe0ea08d2061e1f9b90be65e96
Antony <antony.fernandez.dearsley@gmail.com>
2022-04-28 16:02:33 +0200

    Adding Menu to Home Page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { useLocation } from 'wouter'
+import Menu from '../../components/Menu'
+import "./index.css"
 
 export default function Home() {
     const [keyword, setKeyword] = useState('')
@@ -14,9 +16,14 @@ export default function Home() {
     const handleChange = e => {
         setKeyword(e.target.value)
     }
-    return  <div id='container' className='bg-zinc-900 h-screen  flex justify-center items-center'>
-                <form onSubmit={handleSubmit}>
-                    <input placeholder="Search a film here..." onChange={handleChange} type='text' value={keyword}  className='rounder focus:none'/>
-                </form>
+
+    return  <div id='container' className='home'>
+                <Menu/>
+                <div className='border w-screen flex justify-center items-center'>
+                    <form onSubmit={handleSubmit}>
+                        <input placeholder="Search a title here..." onChange={handleChange} 
+                        type='text' value={keyword} className='rounded-full p-2 w-96'/>
+                    </form>
+                </div>
             </div>
 }
