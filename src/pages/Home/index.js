22c755d34575024106831d2e541893597d9e9a8f
Antony <antony.fernandez.dearsley@gmail.com>
2022-04-28 21:32:04 +0200

    adding icons

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import { useLocation } from 'wouter'
 import Menu from '../../components/Menu'
 import "./index.css"
+import { faSearch } from '@fortawesome/fontawesome-free-solid'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Home() {
     const [keyword, setKeyword] = useState('')
@@ -19,10 +21,13 @@ export default function Home() {
 
     return  <div id='container' className='home'>
                 <Menu/>
-                <div className='border w-screen flex justify-center items-center'>
-                    <form onSubmit={handleSubmit}>
-                        <input placeholder="Search a title here..." onChange={handleChange} 
-                        type='text' value={keyword} className='rounded-full p-2 w-96'/>
+                <div className='h-screen sm:w-screen flex justify-center items-center'>
+                    <form onSubmit={handleSubmit} className="relative">
+                        <input placeholder=" Search a title here..." onChange={handleChange} 
+                        type='text' value={keyword} className='rounded-full p-3 w-[20rem] outline-double 
+                        focus:outline-8 focus:outline-white font-Montserrat' />
+                        <FontAwesomeIcon icon={faSearch} className="text-black text-xl absolute 
+                        right-5 top-3"/>
                     </form>
                 </div>
             </div>
